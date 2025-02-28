934c7141ac69d725380524b6c6d416acb02ea375
Anthony Badea <abadea@fasic-137645.fnal.gov>
2025-02-27 23:33:45 -0600

    updating style

--- lab_analysis/GaussianNoise.py
+++ lab_analysis/GaussianNoise.py
@@ -8,6 +8,8 @@ import mplhep as hep
 # plt.style.use(hep.style.ROOT)
 hep.style.use("ATLAS")
 
+from SmartPixStyle import *
+
 # Gaussian function to fit
 def gaussian(x, amplitude, mean, std_dev):
     return amplitude * np.exp(-((x - mean)**2) / (2 * std_dev**2))
@@ -34,24 +36,27 @@ outDir = os.path.dirname(inFile)
 
 pltConfig = {}
 pltConfig["nelectron_asic_50perc_perBit"] = {
-    "xlabel": "Number of Electrons at 50% S-Curve", 
-    "ylabel": "Number of Bits",
-    "binConfigs": [[0, 800, 81], [800, 1800, 101], [2000, 3600, 161]], # bit 0, bit 1, bit 2
-    "p0s": [[50, 400, 100], [50, 1200, 100], [50, 2800, 100]] # bit 0, bit 1, bit 2
+    "xlabel": r"S-Curve Half Max [e$^{-}$]", 
+    "ylabel": r"N$_{\mathrm{Bits}}$",
+    "binConfigs": [[0, 800, 41], [800, 1600, 41], [2000, 3400, 71]], # bit 0, bit 1, bit 2
+    "p0s": [[50, 400, 100], [50, 1200, 100], [50, 2800, 100]], # bit 0, bit 1, bit 2
+    "ylim": [0, 30]
 }
 pltConfig["scurve_mean_perBit"] = {
-    "xlabel": "Mean of S-Curve", 
-    "ylabel": "Number of Bits",
-    "binConfigs": [[0, 800, 81], [800, 1800, 101], [2000, 3600, 161]], # bit 0, bit 1, bit 2
-    "p0s": [[50, 400, 100], [50, 1200, 100], [50, 2800, 100]] # bit 0, bit 1, bit 2
+    "xlabel": r"S-Curve $\mu$ [e$^{-}$]", 
+    "ylabel": r"N$_{\mathrm{Bits}}$",
+    "binConfigs": [[0, 800, 41], [800, 1600, 41], [2000, 3400, 71]], # bit 0, bit 1, bit 2
+    "p0s": [[50, 400, 100], [50, 1200, 100], [50, 2800, 100]], # bit 0, bit 1, bit 2
+    "ylim": [0, 30]
 }
 pltConfig["scurve_std_perBit"] = {
-    "xlabel": "Standard Deviation of S-Curve", 
-    "ylabel": "Number of Bits",
+    "xlabel": r"S-Curve $\sigma$ [e$^{-}$]", 
+    "ylabel": r"N$_{\mathrm{Bits}}$",
     "binConfigs": [[0, 300, 31], [0, 300, 31], [0, 300, 31]], # bit 0, bit 1, bit 2
-    "p0s": None # bit 0, bit 1, bit 2
+    "p0s": None, # bit 0, bit 1, bit 2
+    "ylim": [0, 60]
 }
-# ["nelectron_asic_50perc_perBit", "scurve_mean_perBit", "scurve_std_perBit"]:
+
 for name, config in pltConfig.items():
     for iB in range(3):
         
@@ -63,34 +68,53 @@ for name, config in pltConfig.items():
         # set up figure
         fig, ax = plt.subplots(figsize=(6,6))
         ax.set_xlabel(config["xlabel"], fontsize=18, labelpad=10)
-        ax.set_ylabel(config["ylabel"], fontsize=18, labelpad=10)
+        ax.set_ylabel(config["ylabel"] + f" / {bins[1]-bins[0]}" + r" e$^{-}$", fontsize=18, labelpad=10)
         
         # plot
-        ax.hist(inData[name][iB], bins=bins, histtype="step") # plot data histogram
+        hist_vals, bin_edges = np.histogram(inData[name][iB], bins=bins, density=False)
+        ax.hist(inData[name][iB], bins=bins, histtype="step", linewidth=1.5, color='black', label='Data') # plot data histogram
         
         # gaussian fit
         if config["p0s"] is not None:
-            hist_vals, bin_edges = np.histogram(inData[name][iB], bins=bins, density=False)
             bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
             popt, _ = curve_fit(gaussian, bin_centers, hist_vals, p0=config["p0s"][iB]) # fit gaussian
+            amplitude, mean , std_dev = popt
             y_fit = gaussian(bin_centers, *popt) # evaluate gaussian at bins
-            ax.plot(bin_centers, y_fit, color='r', label='Fitted Gaussian') # fit
+            ax.plot(bin_centers, y_fit, color='r', label='Gaussian Fit''\n'fr'({mean:.2f},{std_dev:.2f})') # fit
         
+        # add legend
+        legend = ax.legend(fontsize=12)
+        for text in legend.get_texts():
+            text.set_fontweight('bold')
+
         # limits
         ax.set_xlim(bins[0], bins[-1])
+        if config["ylim"] is not None:
+            ax.set_ylim(config["ylim"])
+        else:
+            ax.set_ylim(0, 1.25 * max(hist_vals))
+
+
         # ax.set_ylim(-0.05, 1.05)
         # Set up ticks
         ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
         ax.yaxis.set_major_locator(ticker.MaxNLocator(nbins=5))
         ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
         ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-        ax.tick_params(axis='both', which='major', labelsize=14, length=5, width=1, direction="in")
-        ax.tick_params(axis='both', which='minor', labelsize=10, length=3, width=1, direction="in")
+        ax.tick_params(axis='both', which='major', labelsize=18, length=8, width=1, direction="in", pad=8)
+        ax.tick_params(axis='both', which='minor', labelsize=18, length=4, width=1, direction="in", pad=8)
 
+        # add label and text
+        SmartPixLabel(ax, 0.05, 0.9, size=22)
+        ax.text(0.05, 0.85, "ROIC V1, ID 11, SuperPixel 2", transform=ax.transAxes, fontsize=12, color="black", ha='left', va='bottom')
         # Add text to the plot showing fitted Gaussian parameters
         amplitude, mean , std_dev = popt
-        text = f'Bit = {iB}''\n'fr'Amplitude = {amplitude:.2f}''\n'fr'$\mu$ = {mean:.2f}''\n'fr'$\sigma$ = {std_dev:.2f}' if config["p0s"] is not None else f'Bit = {iB}'
-        ax.text(0.9, 0.90, text, transform=ax.transAxes, fontsize=12, color="black", ha='right', va='center')
+        # text = f'Bit = {iB}''\n'fr'Amplitude = {amplitude:.2f}''\n'fr'$\mu$ = {mean:.2f}''\n'fr'$\sigma$ = {std_dev:.2f}' if config["p0s"] is not None else f'Bit = {iB}'
+        # ax.text(0.9, 0.90, text, transform=ax.transAxes, fontsize=12, color="black", ha='right', va='center')
+
+        
+
+       
 
         # save fig
         outFileName = os.path.join(outDir, f"{name}_Bit{iB}.pdf")
