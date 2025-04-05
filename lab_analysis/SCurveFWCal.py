660996ec5ee5178c2ed2d4deabc0a9923d8abfc6
Benjamin P <bparpill@fnal.gov>
2025-04-04 19:27:11 -0500

    first commit of analysis of large number of settings

---
+++ lab_analysis/SCurveFWCal.py
@@ -0,0 +1,56 @@
+import numpy as np 
+from scipy.optimize import curve_fit
+import os
+import matplotlib.pyplot as plt 
+import matplotlib.ticker as ticker
+import mplhep as hep
+import argparse
+
+# Argument parser
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("-i", '--inFilePath', type=str, required=True, help='Input file path')
+parser.add_argument("-o", '--outDir', type=str, default=None, help='Input file path')
+args = parser.parse_args()
+parent_directory = "/mnt/local/CMSPIX28/Scurve/data/ChipVersion1_ChipID9_SuperPix2/2025.04.03_17.32.32_MatrixCalibration_vMin0.001_vMax0.400_vStep0.03400_nSample32.000_vdda0.900_VTH0.800_BXCLK10.00/"
+FileSetting = "settings.npy"
+file_path = os.path.join(parent_directory, FileSetting)
+# input file
+
+inData = np.load(args.inFilePath)
+features = inData["features"]
+nelectron_asics = inData["nelectron_asics"]
+scurve = inData["scurve"]
+print(scurve.shape)
+settings = np.load(file_path)
+print(settings.shape)
+# mu_c is the pixel count per settings that respect the condition mu_bit2 > mu_bit1 > mu_bit0 > 0  
+mu_c = np.zeros((features.shape[0]))
+fifty_c = np.zeros((features.shape[0]))
+
+for i in range(features.shape[0]):
+    mu_c[i] = np.count_nonzero((features[i,:,2,2]>features[i,:,1,2]) & (features[i,:,1,2]>features[i,:,0,2]) & (features[i,:,0,2]>0))
+    fifty_c[i] = np.count_nonzero((features[i,:,2,1]>features[i,:,1,1]) & (features[i,:,1,1]>features[i,:,0,1]) & (features[i,:,0,1]>0))
+
+bestSettingResult = np.max(mu_c)
+best_settings = np.argmax(mu_c)
+
+mu_c = mu_c.reshape(40, 40)
+print(settings.shape)
+testSample = settings[:,4]
+testDelay = settings[:,5]
+print(testSample.shape)
+
+# Create the plot
+plt.scatter(testSample, testDelay, c=mu_c, cmap='viridis', s=256, edgecolor='black')  # Use a colormap
+
+# Add labels
+plt.xlabel('cfg_test_delay')
+plt.ylabel('cfg_test_sample')
+plt.title('2D Plot of working settings')
+
+# Show the plot
+plt.colorbar(label='Values')  # Display color bar to indicate values
+plt.show()
+
+
+
